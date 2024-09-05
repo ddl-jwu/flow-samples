4b90592a8235c2aa9512635f49bae03d27457d0e
John Wu <john.wu@dominodatalab.com>
2024-09-05 22:36:42 +0000

    Removed hardcoded example

--- workflow.py
+++ workflow.py
@@ -1,16 +1,14 @@
 from flytekit import workflow
 from flytekit.types.file import FlyteFile
-from typing import TypeVar, NamedTuple
 from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
-from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
 
 # pyflyte run --remote workflow.py variable_inputs --data_path_a /mnt/data/snapshots/DatasetA/1 --data_path_b /mnt/data/snapshots/DatasetB/1 --data_path_c /mnt/data/snapshots/DatasetC/1
 @workflow
-def variable_inputs(data_path_a: str, data_path_b: str, data_path_c: str) -> FlyteFile[TypeVar("pdf")]:
+def multiple_inputs(data_path_a: str, data_path_b: str, data_path_c: str) -> FlyteFile[TypeVar("pdf")]:
 
     data_outputs = run_domino_job_task(
         flyte_task_name="Merge data",
-        command="prod/adam-variable.sas",
+        command="prod/adam.sas",
         environment_name="SAS Analytics Pro",
         inputs=[
             Input(name="data_path_a", type=str, value=data_path_a),
@@ -23,36 +21,7 @@ def variable_inputs(data_path_a: str, data_path_b: str, data_path_c: str) -> Fly
 
     report_outputs = run_domino_job_task(
         flyte_task_name="Create report",
-        command="prod/tfl_report.sas",
-        environment_name="SAS Analytics Pro",
-        inputs=[Input(name="adam", type=FlyteFile[TypeVar("sas7bdat")], value=data_outputs["adam"])],
-        output_specs=[Output(name="report", type=FlyteFile[TypeVar("pdf")])],
-        use_project_defaults_for_omitted=True
-    )
-
-    return report_outputs["report"]
-
-# pyflyte run --remote workflow.py hardcoded_inputs
-@workflow
-def hardcoded_inputs() -> FlyteFile[TypeVar("pdf")]:
-
-    data_outputs = run_domino_job_task(
-        flyte_task_name="Merge data",
-        command="prod/adam-hardcoded.sas",
-        environment_name="SAS Analytics Pro",
-        dataset_snapshots=[
-            DatasetSnapshot(Id="66da0221e651b51a171f2e47", Version=1), # DatasetA
-            DatasetSnapshot(Id="66da023ee651b51a171f2e4c", Version=1), # DatasetB
-            DatasetSnapshot(Id="66da02604d3b8b676a1b9460", Version=1) # DatasetC
-        ],
-        inputs=None,
-        output_specs=[Output(name="adam", type=FlyteFile[TypeVar("sas7bdat")])],
-        use_project_defaults_for_omitted=True
-    )
-
-    report_outputs = run_domino_job_task(
-        flyte_task_name="Create report",
-        command="prod/tfl_report.sas",
+        command="prod/tfl.sas",
         environment_name="SAS Analytics Pro",
         inputs=[Input(name="adam", type=FlyteFile[TypeVar("sas7bdat")], value=data_outputs["adam"])],
         output_specs=[Output(name="report", type=FlyteFile[TypeVar("pdf")])],
